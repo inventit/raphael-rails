f499274cc059884450917523839e7ab741c9fe4b
Toshiyuki HINA <thina@yourinventit.com>
2019-02-04 16:46:28 +0900

    initial commit

---
+++ lib/raphael/rails.rb
@@ -0,0 +1,8 @@
+require "raphael/rails/version"
+
+module Raphael
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/raphael/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Raphael::Rails do
+  it "has a version number" do
+    expect(Raphael::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
